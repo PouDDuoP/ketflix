e366cb85acb705454c2074992aa20af430b9ab62
PouDDuoP <kevinalvarado.ag@gmail.com>
2023-07-14 00:09:52 -0400

    ViewSet y ModelViewSet

--- series/api/serializers.py
+++ series/api/serializers.py
@@ -2,17 +2,64 @@
 from typing import Dict
 from django.forms import ValidationError
 from rest_framework import serializers
-from series.models import Serie
+from series.models import Episode, Serie
+
+
+# class SerieSerializer(serializers.ModelSerializer):
+    # id = serializers.IntegerField(read_only=True)
+    # title = serializers.CharField(required=True)
+    # description = serializers.CharField(required=True)
+    
+    # def validate(self, serie: Dict[str, str]):
+    #     if not serie.get('title'):
+    #         raise ValidationError('Title is mandatory')
+    #     return serie
+    
+    # def validate_title(self, title: str):
+    #     series = Serie.objects.filter(title=title)
+    #     if series.exists():
+    #         raise ValidationError('The title already exists')
+    #     return title
+    
+    # def validate_description(self, description: str):
+    #     if not description:
+    #         raise ValidationError('The description cannot ne blank')
+    #     return description
+    
+    # def create(self, **kwargs) -> Serie:
+    #     serie = Serie.objects.create(**self.validated_data)
+    #     return serie
+    
+    # def update(self, **kwargs) -> Serie:
+    #     for attr, value in self._validated_data.items():
+    #         setattr(self.instance, attr, value)
+            
+    #     self.instance.save()
+    #     return self.instance()
+    
+    # def save(self, **kwargs):
+    #     if not self.instace:
+    #         self.instance = self.create()
+    #     elif self.instance:
+    #         self.instance = self.update()
+            
+    #     return self.instance
 
 
 class SerieSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    
-    def validate(self, serie: Dict[str, str]):
-        if not serie.get('title'):
-            raise ValidationError('Title is mandatory')
-        return serie
     
     class Meta:
         model = Serie
-        fields = '__all__'
+        fields = ('id', 'title', 'description')
+        
+class EpisodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Episode
+        fields = ('id', 'name')
+        
+class DetailSeriesSerializer(serializers.ModelSerializer):
+    episodes = EpisodeSerializer(source='episode_set', many=True)
+    
+    class Meta:
+        model = Serie
+        fields = ('id', 'title', 'description', 'episodes')
--- series/api/views.py
+++ series/api/views.py
@@ -3,29 +3,50 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from series.api.serializers import SerieSerializer
+from series.api.serializers import SerieSerializer, DetailSeriesSerializer
 
 from series.models import Serie
 
 
 # class SerieApiView(APIView):
 # class SerieApiView(viewsets.ViewSet):
+# class SerieApiView(viewsets.ModelViewSet):
+#     permission_classes = [IsAuthenticated]
+    
+#     queryset = Serie.objects.all()
+#     serializer_class = SerieSerializer
+    
+#     def list(self, request):
+#         series = SerieSerializer(Serie.objects.all(), many=True)
+#         return Response(data=series.data, status=status.HTTP_200_OK)
+    
+#     def retrieve(self, request, pk: int):
+#         series = SerieSerializer(Serie.objects.get(pk=pk))
+#         return Response(data=series.data, status=status.HTTP_200_OK)
+    
+#     def create(self, request):
+#         serie_serializer = SerieSerializer(data=request.POST)
+#         serie_serializer.is_valid(raise_exception=True)
+        
+#         serie_serializer.save()
+#         return self.list(request)
+
+    # def update(self, request):
+    #     serie_serializer = SerieSerializer(data=request.POST)
+    #     serie_serializer.is_valid(raise_exception=True)
+        
+    #     serie_serializer.save()
+    #     return self.list(request)
+
 class SerieApiView(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
-    
-    queryset = Serie.objects.all()
     serializer_class = SerieSerializer
+    queryset = Serie.objects.all()
     
-    def list(self, request):
-        series = SerieSerializer(Serie.objects.all(), many=True)
-        return Response(data=series.data, status=status.HTTP_200_OK)
-    
-    def retrieve(self, request, pk: int):
-        series = SerieSerializer(Serie.objects.get(pk=pk))
-        return Response(data=series.data, status=status.HTTP_200_OK)
-    
-    def create(self, request):
-        serie = SerieSerializer(data=request.POST)
-        serie.is_valid(raise_exception=True)
-        Serie.objects.create(title=serie.validated_data['title'], description=serie.validated_data['description'])
-        return self.list(request)
+    def get_serializer_class(self):
+        serializer = self.serializer_class
+        
+        if self.action == 'retrieve':
+            serializer = DetailSeriesSerializer  
+        
+        return serializer
