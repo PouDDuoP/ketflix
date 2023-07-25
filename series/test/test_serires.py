1b6d2b644c789a77b557c45c59525350c012c56a
PouDDuoP <kevinalvarado.ag@gmail.com>
2023-07-25 08:46:16 -0400

    ultimos cabios para utilizar TEST

--- series/test/test_serires.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-
-class TestSerie(TestCase):
-    def test_retrieve_serie(self):
-        response = self.client.get(f'/api/series/')
-        
-        self.assertEqual(response.status_code, 200)
