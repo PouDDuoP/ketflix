ad8fbabb8fb765c5b70fc55f3d460bfa40cba78b
PouDDuoP <kevinalvarado.ag@gmail.com>
2023-07-27 09:59:55 -0400

    Testing - Autenticando usuario 1

--- series/tests.py
+++ series/tests.py
@@ -1,13 +1,20 @@
 from uuid import uuid4
 from django.test import TestCase
 from series.models import Episode, Serie
+from django.contrib.auth.models import User
+
+from rest_framework import status
+
 
 # Create your tests here.
 
 class TestSerie(TestCase):
     
+    def _generate_user(self) -> User:
+        return User.objects.create(username=f'fake username {uuid4()}', password='megaclave', email=f'fake email {uuid4()}')
+    
     def _generate_serie(self):
-        serie = Serie.objects.create(title=f'mock serie {uuid4()}', description='mock description')
+        serie = Serie.objects.create(title=f'mock serie {uuid4()}', description=f'mock description')
         
         for i in range(0, 6):
             Episode.objects.create(serie_id=serie.pk, name=f'mock episode {uuid4()}', number=i+1)
@@ -16,8 +23,7 @@ class TestSerie(TestCase):
     
     def test_retrieve_serie(self):
         serie = self._generate_serie()
-        
-        
+
         response = self.client.get(f'/api/series/{serie.pk}/')
         
         self.assertEqual(response.status_code, 200)
@@ -31,3 +37,19 @@ class TestSerie(TestCase):
         self.assertIsInstance(response_json.get('episodes'), list)
 
         self.assertEqual(len(response_json.get('episodes')), 6)
+        
+    def test_create_serie(self):
+        user = self._generate_user()
+        self.client.force_login(user)
+        
+        serie_dict = {'title':f'mock serie {uuid4()}', 'description':f'mock description'}
+        response = self.client.post(f'/api/series/', serie_dict)
+        
+        response_json = response.json()
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        self.assertIsInstance(response_json, dict)
+        self.assertIsInstance(response_json.get('id'), int)
+        self.assertIsInstance(response_json.get('title'), str)
+        self.assertIsInstance(response_json.get('description'), str)
